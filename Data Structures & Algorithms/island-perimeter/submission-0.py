fc6b902917e502675375692d4abe397768f9bb84
Ribash Sharma <37812264+rebussharma@users.noreply.github.com>
2026-06-01 16:57:59 -0500

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        perimeter = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    perimeter += 4
+
+                    # Shared edge with upper cell
+                    if r > 0 and grid[r - 1][c] == 1:
+                        perimeter -= 2
+
+                    # Shared edge with left cell
+                    if c > 0 and grid[r][c - 1] == 1:
+                        perimeter -= 2
+
+        return perimeter
